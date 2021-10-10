fabd84f3b8b4b988d5e0ff6eb9e72a4e5a0e92fa
kylelawrencedev <kylelawrence19@gmail.com>
2021-10-10 09:26:41 +0000

    updated contexts processor for bag app

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
@@ -9,6 +8,29 @@ def bag_contents(requst):
     bag_items = []
     total = 0
     product_count = 0
+    bag = requst.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     grand_total = total
 
