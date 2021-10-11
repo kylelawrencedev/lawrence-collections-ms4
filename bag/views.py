424446d47d6f1a5a685635fbd39d0604537574a1
kylelawrencedev <kylelawrence19@gmail.com>
2021-10-11 17:02:45 +0000

    Added functionality for toasts

--- bag/views.py
+++ bag/views.py
@@ -40,7 +40,8 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
             messages.success(
-                request, f'Updated "{product.name}" quantity to {bag[item_id]}')
+                request, f'Updated "{product.name}" quantity to {bag[item_id]}'
+                )
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added "{product.name}"to your bag')
