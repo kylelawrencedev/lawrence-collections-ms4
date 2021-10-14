b6aa02d51611ac60a2549558be6f21eabcbbb12d
kylelawrencedev <kylelawrence19@gmail.com>
2021-10-14 14:51:13 +0000

    Removed secret key from settings

--- lawrence_collections/settings.py
+++ lawrence_collections/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yvc$%%6%p=ir8h&%x65%8sz^#vlm5!lf)a4=fzwu$y0(^pyw(2'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
