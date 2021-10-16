5ba9ae4ba006cf4ea56e6a69a3b12c8cbedd4392
kylelawrencedev <kylelawrence19@gmail.com>
2021-10-16 18:18:34 +0000

    Completed stripe webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
-# Register your models here.
-
 
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
@@ -12,15 +10,16 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date',
-                       'order_total', 'grand_total',
-                       'original_bag')
+    readonly_fields = ('order_number', 'date', 'order_total',
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county','order_total',
-              'grand_total',)
+              'street_address2', 'county', 'order_total',
+              'grand_total', 'original_bag',
+              'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'grand_total',)
