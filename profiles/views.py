ca7b108b6e6a00c1e2c8b3876eebfe5724c638ff
kylelawrencedev <kylelawrence19@gmail.com>
2021-10-18 12:17:28 +0000

    Bug: Updated views in profiles

--- profiles/views.py
+++ profiles/views.py
@@ -6,9 +6,8 @@ from .forms import UserProfileForm
 
 from checkout.models import Order
 
-
 def profile(request):
-    """ Display the user's profile """
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -16,17 +15,15 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(request, 'Update failed. Please ensure form is valid.')
-    else:
-        form = UserProfileForm(instance=profile)
+
+    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
