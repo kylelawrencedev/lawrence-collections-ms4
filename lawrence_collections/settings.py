0300825a63ed4d5a727ed226db27b1358deb4e41
kylelawrencedev <kylelawrence19@gmail.com>
2021-10-30 22:42:05 +0000

    Update email address to custom email

--- lawrence_collections/settings.py
+++ lawrence_collections/settings.py
@@ -218,7 +218,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'kylelawrence19@gmail.com'
+    DEFAULT_FROM_EMAIL = 'lawrencecollectionsms4@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
